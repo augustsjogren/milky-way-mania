d55e093a2c65493101d18c1e835948057f28a943
August Sjögren <augsj305@student.liu.se>
2018-01-04 17:36:51 +0100

    Code cleanup and lots of comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Set some of the sliders to initial values
 function init(){
   document.getElementById("amp-slider").value = 0.0;
   document.getElementById("amp-small-slider").value = 0.0;
@@ -15,13 +16,11 @@ var ASPECT = WIDTH / HEIGHT;
 var NEAR = 1;
 var FAR = 100000;
 
-// get the DOM element to attach to
-// - assume we've got jQuery to hand
+// get the DOM element to attach to using jQuery
 var $container = $('#container');
 var $performance = $('#performance');
 
-// Create a WebGL renderer, camera
-// and a scene
+// Create a WebGL renderer, camera and a scene
 var renderer = new THREE.WebGLRenderer();
 var camera = new THREE.PerspectiveCamera(
   VIEW_ANGLE,
@@ -30,10 +29,12 @@ var camera = new THREE.PerspectiveCamera(
   FAR
 );
 
+// This is just to supress a lot of warnings, some kind of bug
 var ctx = renderer.context;
 // shut firefox up
 ctx.getShaderInfoLog = function () { return '' };
 
+// Controls to navigate the scene
 var controls = new THREE.TrackballControls( camera );
 controls.rotateSpeed = 1.5;
 controls.zoomSpeed = 1.2;
@@ -45,20 +46,17 @@ controls.dynamicDampingFactor = 0.1;
 controls.keys = [ 65, 83, 68 ];
 controls.addEventListener( 'change', render );
 
-
 var orbitControls = new THREE.OrbitControls( camera, renderer.domElement );
 orbitControls.addEventListener( 'change', render ); // remove when using animation loop
-// enable animation loop when using damping or autorotation
-//controls.enableDamping = true;
-//controls.dampingFactor = 0.25;
 orbitControls.enableZoom = false;
 
+// Create the scene
 var scene = new THREE.Scene();
 
-// the camera starts at 0,0,0 so pull it back
+// Move the camera back towards the viewer
 camera.position.z = 1000;
 
-// Start the renderer.
+// Setup the renderer.
 renderer.setSize(WIDTH, HEIGHT);
 
 // Attach the renderer-supplied DOM element.
@@ -93,64 +91,58 @@ pointLight.position.set(100, 0, 100);
 scene.add( pointLight );
 //scene.add( light );
 
-var uniforms2 = THREE.UniformsUtils.merge( [ THREE.UniformsLib[ "lights" ] ] );
-uniforms2.smallAmplitude = {  type:'f', value:1 };
-uniforms2.amplitude = { type: 'f', value: 1 };
-uniforms2.valleys = { type: 'f', value: 1 };
-uniforms2.snowLevel = { type: 'f', value: 1 };
-uniforms2.vegetation = { type: 'f', value: 1 };
-uniforms2.planetRadius = { type: 'f', value: 1 };
-uniforms2.waterLevel = { type: 'f', value: 1 };
-
-// The offset for sending to shaders
+// The planet offset for sending to shaders
 var planet1Trans = new THREE.Vector3(600.0, 0.0, 0.0);
 var planet2Trans = new THREE.Vector3(900.0, 0.0, 0.0);
-
-var wateruniforms = THREE.UniformsUtils.merge( [ THREE.UniformsLib[ "lights" ] ] );
-wateruniforms.waterLevel = { type: 'f', value: 1 };
-wateruniforms.planetRadiusWater = { type: 'f',  value: 1 };
-wateruniforms.lightPosition = { type: 'v3', value: new THREE.Vector3(0.0, 0.0, 0.0) };
-wateruniforms.planetTrans = { type: 'v3', value: planet2Trans };
-
-function render() {
-  // Draw!
-  renderer.render(scene, camera);
-}
-
-uniforms2.randomSeed = {
-  type: 'f',
-  value: Math.random() * 1.0 + 0.7
-};
-
-uniforms2.planetTrans = {
-  type: 'v3',
-  value: planet2Trans
-};
-
-
-var sununiforms = THREE.UniformsUtils.merge( [ THREE.UniformsLib[ "lights" ] ] );
-sununiforms.waterLevel = { type: 'f', value: 1 };
-sununiforms.planetRadiusWater = { type: 'f', value: 1 };
-sununiforms.lightPosition = { type: 'v3', value: new THREE.Vector3(0.0, 0.0, 0.0) };
-sununiforms.time = { type: 'f', value: 0.0 };
 
 // Set up the sphere vars
 var RADIUS = 80;
 var SEGMENTS = 512;
 var RINGS = 512;
 
+// Planet 1 uniforms
 var uniforms = THREE.UniformsUtils.merge( [ THREE.UniformsLib[ "lights" ] ] );
 uniforms.smallAmplitude = { type: 'f', value: 1 };
-uniforms.amplitude=       { type: 'f', value: 1 };
-uniforms.valleys = { type: 'f', value: 1 };
-uniforms.snowLevel = { type: 'f', value: 1 };
-uniforms.vegetation = { type: 'f', value: 1 };
-uniforms.planetRadius = { type: 'f', value: 1 };
-uniforms.waterLevel = { type: 'f', value: 1 };
-uniforms.randomSeed = { type: 'f', value: Math.random() * 1.0 + 0.7 };
-uniforms.planetTrans = { type: 'v3',value: planet1Trans };
-uniforms.radius = { type: 'f', value: RADIUS };
-uniforms.lightPosition = { type: 'v3',value: new THREE.Vector3(10.0, 0.0, 0.0) };
+uniforms.amplitude      = { type: 'f', value: 1 };
+uniforms.valleys        = { type: 'f', value: 1 };
+uniforms.snowLevel      = { type: 'f', value: 1 };
+uniforms.vegetation     = { type: 'f', value: 1 };
+uniforms.planetRadius   = { type: 'f', value: 1 };
+uniforms.waterLevel     = { type: 'f', value: 1 };
+uniforms.randomSeed     = { type: 'f', value: Math.random() * 1.0 + 0.7 };
+uniforms.planetTrans    = { type: 'v3',value: planet1Trans };
+uniforms.radius         = { type: 'f', value: RADIUS };
+uniforms.lightPosition  = { type: 'v3',value: new THREE.Vector3(10.0, 0.0, 0.0) };
+
+// Planet 2 uniforms
+var uniforms2 = THREE.UniformsUtils.merge( [ THREE.UniformsLib[ "lights" ] ] );
+uniforms2.smallAmplitude = {  type:'f', value:1 };
+uniforms2.amplitude      = { type: 'f', value: 1 };
+uniforms2.valleys        = { type: 'f', value: 1 };
+uniforms2.snowLevel      = { type: 'f', value: 1 };
+uniforms2.vegetation     = { type: 'f', value: 1 };
+uniforms2.planetRadius   = { type: 'f', value: 1 };
+uniforms2.waterLevel     = { type: 'f', value: 1 };
+uniforms2.randomSeed     = { type: 'f', value: Math.random() * 1.0 + 0.7 };
+uniforms2.planetTrans    = { type: 'v3', value: planet2Trans };
+
+// Water uniforms
+var wateruniforms = THREE.UniformsUtils.merge( [ THREE.UniformsLib[ "lights" ] ] );
+wateruniforms.waterLevel        = { type: 'f', value: 1 };
+wateruniforms.planetRadiusWater = { type: 'f',  value: 1 };
+wateruniforms.lightPosition     = { type: 'v3', value: new THREE.Vector3(0.0, 0.0, 0.0) };
+wateruniforms.planetTrans       = { type: 'v3', value: planet2Trans };
+
+// Sun uniforms
+var sununiforms = THREE.UniformsUtils.merge( [ THREE.UniformsLib[ "lights" ] ] );
+sununiforms.waterLevel        = { type: 'f', value: 1 };
+sununiforms.planetRadiusWater = { type: 'f', value: 1 };
+sununiforms.lightPosition     = { type: 'v3', value: new THREE.Vector3(0.0, 0.0, 0.0) };
+sununiforms.time              = { type: 'f', value: 0.0 };
+
+function render() {
+  renderer.render(scene, camera);
+}
 
 // Load the shaders from files
 ShaderLoader("shaders/vertex_planet.vert", "shaders/fragment_planet.frag",
@@ -160,9 +152,7 @@ function (vertex, fragment) {
   // create the sphere's material
   var shaderMaterial = new THREE.ShaderMaterial({
     uniforms:       uniforms,
-    // vertexShader:   $('#vertexshader').text(),
     vertexShader:   vertex,
-    // fragmentShader: $('#fragmentshader').text(),
     fragmentShader: fragment,
     lights:         true,
     wireframe:      false
@@ -171,9 +161,7 @@ function (vertex, fragment) {
   // create the sphere's material
   var shaderMaterial2 = new THREE.ShaderMaterial({
     uniforms:       uniforms2,
-    // vertexShader:   $('#vertexshader').text(),
     vertexShader:   vertex,
-    // fragmentShader: $('#fragmentshader').text(),
     fragmentShader: fragment,
     lights:         true,
     wireframe:      false
@@ -193,39 +181,24 @@ function (vertex, fragment) {
     lights:         true
   });
 
-
-
-  // uniforms.radius = {
-  //   type: 'f',
-  //   value: RADIUS
-  // };
-
-  // uniforms.lightPosition = {
-  //   type: 'v3',
-  //   value: new THREE.Vector3(10.0, 0.0, 0.0)
-  // };
-
-  // Create the geometries for the planets and water
-  var geometry = new THREE.SphereBufferGeometry( RADIUS, SEGMENTS, RINGS );
-  var geometry2 = new THREE.SphereBufferGeometry(80, SEGMENTS, RINGS );
-
+  // Create the geometries for the planets
+  var planet1Geometry = new THREE.SphereBufferGeometry( RADIUS, SEGMENTS, RINGS );
+  var planet2Geometry = new THREE.SphereBufferGeometry(80, SEGMENTS, RINGS );
   var sunGeometry = new THREE.SphereBufferGeometry( 110, 128, 128 );
-
+  // Two spheres used for water
   var waterGeometry = new THREE.SphereBufferGeometry( RADIUS, 128, 128 );
   var waterGeometry2 = new THREE.SphereBufferGeometry( 80, 128, 128 );
 
-  // Add materials and water spheres
-  planet1 = new THREE.Mesh( geometry, shaderMaterial );
+  // Add materials and spheres
+  planet1 = new THREE.Mesh( planet1Geometry, shaderMaterial );
   waterSphere = new THREE.Mesh( waterGeometry, waterShaderMaterial );
   planet1.add(waterSphere);
 
-  sphere2 = new THREE.Mesh( geometry2, shaderMaterial2 );
+  planet2 = new THREE.Mesh( planet2Geometry, shaderMaterial2 );
   waterSphere2 = new THREE.Mesh( waterGeometry2, waterShaderMaterial );
-  sphere2.add(waterSphere2);
+  planet2.add(waterSphere2);
 
   sunSphere = new THREE.Mesh( sunGeometry, sunShaderMaterial );
-
-  planet1.position.set(0.0, 0.0, 0.0);
 
   // Pivots are used to rotate the planets in orbits
   var parent = new THREE.Object3D();
@@ -235,15 +208,15 @@ function (vertex, fragment) {
 
   // Initial planet offset
   planet1.position.x = planet1Trans.x;
-  sphere2.position.x = planet2Trans.x;
+  planet2.position.x = planet2Trans.x;
 
+  // Attatch the planets to their pivots and parents
   pivot1.add(planet1);
-  pivot2.add(sphere2);
-
+  pivot2.add(planet2);
   parent.add(pivot1);
   parent2.add(pivot2);
 
-  // Add  to the scene.
+  // Add stuff to the scene
   scene.add(sunSphere);
   scene.add(parent);
   scene.add(parent2);
@@ -255,7 +228,6 @@ function (vertex, fragment) {
 
     // Collect performance stats
     stats.begin();
-    //var time = Date.now()/10000000;
 
     var time = clock.getElapsedTime();
 
@@ -268,6 +240,7 @@ function (vertex, fragment) {
       //planet1.rotation.y -= 0.05;
     }
 
+    //------------------Update uniforms----------------------
     // Radius
     uniforms.planetRadius.value = document.getElementById("radius-slider").value;
 
@@ -281,8 +254,7 @@ function (vertex, fragment) {
     uniforms.vegetation.value = document.getElementById('vegetation-slider').value;
     uniforms.waterLevel.value = document.getElementById('water-slider').value;
 
-
-    //------------------Uniforms2-----------------------------------
+    //------------------Update uniforms2----------------------
     // Radius
     uniforms2.planetRadius.value = document.getElementById("radius-slider").value;
 
@@ -296,16 +268,12 @@ function (vertex, fragment) {
     uniforms2.vegetation.value = document.getElementById('vegetation-slider').value;
     uniforms2.waterLevel.value = document.getElementById('water-slider').value;
 
-    //--------------------------------------------------------------
-
+    //------------Update water and sun uniforms---------------
     // Water
     wateruniforms.waterLevel.value = document.getElementById('water-slider').value;
     wateruniforms.planetRadiusWater.value = document.getElementById("radius-slider").value;
-
     // Sun
     sununiforms.time.value = time;
-
-    //console.log(sununiforms.time.value);
 
     // Navigation
     controls.update();
@@ -319,13 +287,8 @@ function (vertex, fragment) {
     requestAnimationFrame(update);
   }
 
-  // function render() {
-  //   // Draw!
-  //   renderer.render(scene, camera);
-  // }
-
   init();
   update();
 
 }
-)
+) // End of shader loader
